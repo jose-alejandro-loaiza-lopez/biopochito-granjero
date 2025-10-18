47f6cfecace8e98b44b4529a6a2da84a0d85069d
Jose Loaiza <jose.loaiza01@uceva.edu.co>
2025-10-18 12:08:53 -0500

    menu test

---
+++ Assets/_Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "Main"; // Nombre de la escena principal
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Abrir opciones próximamente...");
+        // Aquí puedes cargar otro panel o escena con las opciones.
+    }
+}
